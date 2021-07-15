0e29bcd6101cf433f9a8077d7c46650805992f72
qiying_xiao <qiying_xiao@126.com>
2021-07-16 07:03:30 +0800

    up

--- java_base/lc/src/main/java/practice3/removeDuplicates80.java
+++ java_base/lc/src/main/java/practice3/removeDuplicates80.java
@@ -9,23 +9,28 @@ package practice3;
 public class removeDuplicates80 {
 
     public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return 1;
+        }
+
         int p = 0;
-        int q = 1;
-        int index = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (q - p > 1 && nums[p] != nums[q]){
-                nums[p + 1] = nums[q];
+        int index = 1;
+        for (int q = 1; q < nums.length; q++) {
+            if (nums[p] != nums[q]){
+                nums[index++] = nums[q];
                 p = q;
-                q = p + 1;
-            }else {
-                q++;
-                if (q - p > 1) {
-                    index++;
-                }
+                q = p;
+            }else if (q - p > 1 && nums[p] == nums[q]){
+                continue;
+            }else if (q - p <= 1){
+                nums[index++] = nums[q];
             }
         }
 
-        return nums.length - index;
+        return index - 1;
     }
 
 }
